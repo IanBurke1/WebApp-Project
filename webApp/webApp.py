8d99042066e6a04d0c902eff9415193d30f23e4f
Ian Burke <iano.burke@gmail.com>
2016-11-21 13:16:22 +0000

    Updated flask

--- webApp/webApp.py
+++ webApp/webApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__) #Pass in _name_ to help flask determine root path.
 
@@ -8,6 +8,16 @@ app = Flask(__name__) #Pass in _name_ to help flask determine root path.
 def homepage():
     return render_template("homepage.html") #when user goes to this page return this..
 
+@app.route('/bmiCalc', methods=['GET','POST'])
+def bmiCalc():
+    if request.method == 'POST':
+        height = request.form['height']
+        weight = request.form['weight']
+        result = request.form['result']
+
+        return render_template('homepage.html', height=height, weight=weight, result=result)
+
+    return render_template('homepage.html')
 
 if __name__ == '__main__': #quick check to make sure that only run the app whenever this file is called directly
     app.run() #Start this web server
