a2b367e39cf756fb0dc92f98013ef80e28c11e3b
Ian Burke <iano.burke@gmail.com>
2016-11-23 21:14:48 +0000

    Updated

--- webApp/webApp.py
+++ webApp/webApp.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import sqlite3 as sql
+
+#DATABASE = 'data/bmi.db' # Define the database
 
 app = Flask(__name__) #Pass in _name_ to help flask determine root path.
 
@@ -13,11 +16,21 @@ def bmiCalc():
     if request.method == 'POST':
         height = request.form['height']
         weight = request.form['weight']
-        result = request.form['result']
+      #  result = request.form['result']
 
-        return render_template('homepage.html', height=height, weight=weight, result=result)
+       # with sql.connect("bmi.db") as con:
+        #    cur = con.cursor()
+         #   cur.execute("INSERT INTO bmi_results (height, weight) VALUES (?,?)",(height, weight) )
 
-    return render_template('homepage.html')
+#            con.commit()
+
+        return render_template('homepage.html')
+
+    #return render_template('homepage.html')
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__': #quick check to make sure that only run the app whenever this file is called directly
     app.run() #Start this web server
